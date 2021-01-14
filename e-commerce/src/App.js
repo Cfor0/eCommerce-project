af18558d2ef0c56690c00da517c222c5931ae64c
cfor0 <cfor0@outlook.com>
2021-01-14 12:12:22 -0500

    added hambuger icon to navbar with animations

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 
 class App extends Component {
+   
   render() {
     return (
-      <NavBar />
-      );
+     
+        <NavBar />
+      
+    );
   }
-  
+
 }
 
 export default App;
--- e-commerce/src/components/NavBar.js
+++ e-commerce/src/components/NavBar.js
@@ -1,24 +1,54 @@
-import React from 'react'
+import React, { Component } from 'react'
+import '../App.css'
 
-const NavBar = () => {
-    return (
-        <nav>
-            <div className='logo'>
-                <h4>The Nav</h4>
-            </div>
-            <ul className='nav-links'>
-                <li>
-                    <a href='#'>Home</a>
-                </li>
-                <li>
-                    <a href='#'>About</a>
-                </li>
-                <li>
-                    <a href='#'>Products</a>
-                </li>
-            </ul>
-        </nav>
-    )
+class NavBar extends Component {
+
+    navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((ele, index) => {
+            if (ele.style.animation) {
+                ele.style.animation = '';
+            } else {
+                ele.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + .3}s`
+            }
+        })
+
+        burger.classList.toggle('toggle');
+
+    }
+    render() {
+
+
+
+        return (
+            <nav>
+                <div className='logo'>
+                    <h4>The Nav</h4>
+                </div>
+                <ul className='nav-links'>
+                    <li>
+                        <a href='#'>Home</a>
+                    </li>
+                    <li>
+                        <a href='#'>About</a>
+                    </li>
+                    <li>
+                        <a href='#'>Products</a>
+                    </li>
+                </ul>
+                <div className='burger' onClick={this.navSlide}>
+                    <div className='line1'></div>
+                    <div className='line2'></div>
+                    <div className='line3'></div>
+                </div>
+            </nav>
+        )
+    }
 }
 
 export default NavBar;
