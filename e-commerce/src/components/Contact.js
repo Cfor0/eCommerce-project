bc774530989c15f1904ff22a56d648d6376c9153
cfor0 <cfor0@outlook.com>
2021-01-25 16:49:25 -0500

    added contact page

--- e-commerce/src/components/Contact.js
+++ e-commerce/src/components/Contact.js
@@ -8,7 +8,48 @@ const Contact = () => {
         <div className='body'>
             <NavBar></NavBar>
             <div className='content'>
-                <p>test</p>
+                <div className='contact-form-container'>
+                    <form id='contact-form'>
+                        <div className='first-last-container'>
+                            <div className='first-last'>
+                                <label htmlFor="firstName">
+                                    First:
+                                </label>
+                                <br></br>
+                                <input className='first-last-input' id="firstName" type="text" name="firstName" required="" />
+
+                            </div>
+                            <div className='first-last'>
+                                <label htmlFor="lastName">
+                                    Last:
+                                </label>
+                                <br></br>
+                                <input id="lastName" type="text" name="lastName" required="" />
+                                <br></br>
+                            </div>
+                            <br></br>
+                        </div>
+                        <label htmlFor="telephone">
+                            Phone:
+                        </label>
+                        <br></br>
+                        <input id="telephone" type="tel" name="telephone" />
+                        <br></br>
+                        <label htmlFor="email">
+                            Email:
+                        </label>
+                        <br></br>
+                        <input id="email" type="email" name="email" />
+                        <br></br>
+                        <label htmlFor="comments">
+                            Comments:
+                        </label>
+                        <br></br>
+                        <textarea id="comments" type="text" name="comments" />
+                        <br></br>
+                        <button id="submit" type="submit" name="submit">Submit</button>
+                    </form>
+                </div>
             </div>
             <Footer />
         </div>
